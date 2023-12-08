561829bdb016daf6207baace6f5a6c721b61844e
Udit Samant <udit.samant@gmail.com>
2023-12-08 18:29:41 +1100

    Add basic css formatting for units page

--- unitrack/urls.py
+++ unitrack/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    path('units/', views.unit_search, name="unit_search"),
-    path('units/<str:unit_code>', views.unit_page, name="unit_page"),
     path('', views.main_page),
+    path('units/', include("units.urls")),
 ]
--- unitrack/views.py
+++ unitrack/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 
-
 def main_page(request):
     contex = {}
     return render(request, 'index.html', contex)
 
 
-def unit_search(request):
-    contex = {}
-    return render(request, 'unit_search.html', contex)
+
+    
 
 
-def unit_page(request, unit_code):
-    contex = {'unit_code': unit_code}
-    return render(request, 'unit_page.html', contex)
+
+
+
