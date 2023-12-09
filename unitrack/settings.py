9342bb2c10dfabfe88135fc72a57fac5029b1094
Jennifer Tan <jtan8485@uni.sydney.edu.au>
2023-12-09 14:35:08 +1100

    Add url to nav bar

--- unitrack/settings.py
+++ unitrack/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, 'static'),
+# ]
 
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
